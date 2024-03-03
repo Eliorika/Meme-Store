c0a2d7c0fc3d17b4db96f6c5955b5ce0abbe0406
grishuchkov <gri.d.a@ya.ru>
2024-03-03 13:57:53 +0300

    delete old save test methods

--- src/main/java/dev/chipichapa/memestore/usecase/ifc/SaveFileUseCase.java
+++
@@ -1,8 +0,0 @@
-package dev.chipichapa.memestore.usecase.ifc;
-
-import dev.chipichapa.memestore.dto.file.SaveFileResponse;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface SaveFileUseCase {
-    SaveFileResponse save(MultipartFile file);
-}
