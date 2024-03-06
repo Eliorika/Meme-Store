413b964a11e467ffe6f783f35027821e50b86e40
Eliorika <arina.pavlovna.20@gmail.com>
2024-03-06 05:23:51 +0300

    img ext

--- src/main/resources/db/changelog/v-1.0/140-add-status-type-bool.sql
+++ src/main/resources/db/changelog/v-1.0/140-add-status-type-bool.sql
@@ -3,5 +3,7 @@ alter table albums
 
 alter table images drop column status;
 
+alter table images add column extension varchar not null;
+
 alter table albums
-    add column status bool;
+    add column status bool not null;
