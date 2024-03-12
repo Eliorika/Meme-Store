9275170fece8158d4b474e502b56f314df793f76
Sabbat <vanua2451@yandex.ru>
2024-03-12 23:58:09 +0300

    Output of all gallery ids

--- src/main/java/dev/chipichapa/memestore/utils/mapper/AlbumMapper.java
+++ src/main/java/dev/chipichapa/memestore/utils/mapper/AlbumMapper.java
@@ -1,18 +1,26 @@
 package dev.chipichapa.memestore.utils.mapper;
 
 import dev.chipichapa.memestore.domain.entity.Album;
+import dev.chipichapa.memestore.domain.entity.user.User;
 import dev.chipichapa.memestore.domain.model.Gallery;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AlbumMapper {
 
     public static Gallery map(Album album) {
         return new Gallery(
-                album.getAuthor().getId(),
-                null,
+                album.getId(),
+                album.getContributors().stream().map(User::getId).collect(Collectors.toSet()),
                 album.getName(),
                 album.getDescription(),
                 album.getStatus(),
                 false,
                 false);
+    }
+
+    public static List<Gallery> map(List<Album> albums) {
+        return albums.stream().map(AlbumMapper::map).toList();
     }
 }
