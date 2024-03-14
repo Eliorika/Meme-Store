b6a1b050e4e2eadd757c2beb92212a3fee02698e
Rudi <rudi.will.do.it@gmail.com>
2024-03-15 01:29:22 +0300

    update docker and add rabbit config

--- src/main/java/dev/chipichapa/ApplicationConfig.java
+++ src/main/java/dev/chipichapa/ApplicationConfig.java
@@ -1,6 +1,12 @@
 package dev.chipichapa;
 
+import dev.chipichapa.memestore.config.RabbitMQConfig;
+import lombok.RequiredArgsConstructor;
 import org.glassfish.grizzly.threadpool.FixedThreadPool;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,7 +17,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableJpaRepositories
+@ConfigurationPropertiesScan
+@RequiredArgsConstructor
 public class ApplicationConfig {
+
+
+    private final RabbitMQConfig rabbitMQConfig;
 
     @Bean
     public ThreadPoolTaskExecutor taskExecutor() {
@@ -21,6 +32,28 @@ public class ApplicationConfig {
         executor.setQueueCapacity(50);
         executor.initialize();
         return executor;
+    }
+
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(rabbitMQConfig.queue())
+                .build();
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(rabbitMQConfig.exchange());
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(rabbitMQConfig.queue());
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
