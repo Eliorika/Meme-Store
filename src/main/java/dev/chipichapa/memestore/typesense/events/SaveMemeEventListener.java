93319f0dc71b67c9d5fb07be56f34310fec3c3c5
grishuchkov <gri.d.a@ya.ru>
2024-03-23 17:02:21 +0300

    fix: ocr delete

--- src/main/java/dev/chipichapa/memestore/typesense/events/SaveMemeEventListener.java
+++ src/main/java/dev/chipichapa/memestore/typesense/events/SaveMemeEventListener.java
@@ -1,7 +1,5 @@
 package dev.chipichapa.memestore.typesense.events;
 
-import dev.chipichapa.memestore.domain.entity.Image;
-import dev.chipichapa.memestore.ocr.dto.OcrImage;
 import dev.chipichapa.memestore.ocr.service.OcrService;
 import dev.chipichapa.memestore.service.ifc.FileService;
 import dev.chipichapa.memestore.service.ifc.ImageService;
@@ -9,7 +7,6 @@ import dev.chipichapa.memestore.service.ifc.SearchService;
 import dev.chipichapa.memestore.typesense.dto.File;
 import dev.chipichapa.memestore.typesense.dto.SavedMeme;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +15,6 @@ import org.springframework.stereotype.Component;
 public class SaveMemeEventListener {
 
     private final SearchService typesense;
-    private final ImageService imageService;
-    private final FileService fileService;
-    private final OcrService ocrService;
 
     @EventListener
     public void accept(SaveMemeEvent event) {
@@ -28,31 +22,13 @@ public class SaveMemeEventListener {
         typesense.saveOrUpdate(savedMemeToFile(savedMeme));
     }
 
-    public File savedMemeToFile(SavedMeme savedMeme){
+    public File savedMemeToFile(SavedMeme savedMeme) {
         return new File(
                 savedMeme.id(),
                 savedMeme.description().toLowerCase(),
                 savedMeme.title().toLowerCase(),
-                getOcrText(savedMeme).toLowerCase(),
+                "ocr",
                 savedMeme.tags()
         );
-    }
-
-    private String getOcrText(SavedMeme savedMeme) {
-        Image image = imageService.getById(Long.valueOf(savedMeme.id()));
-        String filenameWithExtension = image.getFilenameWithExtension();
-
-        dev.chipichapa.memestore.s3storage.dto.File file = fileService.get(filenameWithExtension);
-
-        if(ocrService.isTokenExpireSoon()){
-            ocrService.requestAndSaveNewToken();
-        }
-
-        String textFromImage = ocrService.extractTextFromImage(new OcrImage(
-                file.getContent(),
-                image.getExtension()
-        ));
-
-        return textFromImage;
     }
 }
